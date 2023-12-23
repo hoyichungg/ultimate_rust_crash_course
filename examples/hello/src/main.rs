97c64b1a1d85e1400ce2f916dfc7b28cb537ed37
heyichingg <heyizhongg@gmail.com>
2023-12-23 11:20:54 +0800

    Module System

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -1,7 +1,15 @@
 use hello::{english, russian, spanish};
+use rand::Rng;
 
 fn main() {
     english::greet();
     spanish::greet();
     russian::greet();
+
+    let mut rng = rand::thread_rng();
+
+    let n1: u8 = rng.gen();
+    let n2: u16 = rng.gen();
+    println!("Random u8: {}", n1);
+    println!("Random u16: {}", n2);
 }
